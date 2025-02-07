721041d206e89252390a59667bd7d9a00047c02b
Ahmad Asaad <ahmad@salsitasoft.com>
2025-02-07 11:12:48 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,6 @@ import ErrorModal from './components/ErrorModal'
 import { fetchCoins } from './features/cryptoSlice'
 import { AppDispatch, RootState } from './store/store'
 
-export interface Coin {
-  id: string
-  symbol: string
-  name: string
-  image: string
-  current_price: number
-  price_change_percentage_24h: number
-  sparkline_in_7d: { price: number[] }
-}
-
 function App() {
   const dispatch = useDispatch<AppDispatch>()
   const { loading, error } = useSelector((state: RootState) => state.crypto)
